78820d510cdde9b3b2ece7bd0f26bd852d390979
Jeff Glanville <jeff.glanville@outlook.com>
2019-12-03 22:23:35 -0600

    update index.js

--- js/index.js
+++ js/index.js
@@ -39,7 +39,45 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
-const myNav = document.getElementByClassName('nav-item');
+let myImg = document.getElementById("cta-img");
+myImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let newNav = document.querySelector('nav');
+let link1 = document.createElement('a');
+link1.textContent = 'Services';
+newNav.appendChild(link1);
+
+let newNav1 = document.querySelector('nav');
+let link2 = document.createElement('a');
+link2.textContent = 'Product';
+newNav.appendChild(link2);
+
+let newNav2 = document.querySelector('nav');
+let link3 = document.createElement('a');
+link3.textContent = 'Vision';
+newNav.appendChild(link3);
+
+let newNav3 = document.querySelector('nav');
+let link4 = document.createElement('a');
+link4.textContent = 'Features';
+newNav.appendChild(link4);
+
+let newNav4 = document.querySelector('nav');
+let link5 = document.createElement('a');
+link5.textContent = 'About';
+newNav.appendChild(link5);
+
+let newNav5 = document.querySelector('nav');
+let link6 = document.createElement('a');
+link6.textContent = 'Contact';
+newNav.appendChild(link6);
+
+
+
+
+
+
+
 
